35022bcc732150b323ad4a5c1d0a4cf769b6ef69
wangxianbiao <422675260@qq.com>
2020-12-31 18:40:42 +0800

    sort

--- Golang/go/Sort/HeapSort.go
+++ Golang/go/Sort/HeapSort.go
@@ -1,4 +1,4 @@
-package main
+package sort
 
 import "fmt"
 
@@ -7,30 +7,50 @@ import "fmt"
 
 */
 
-func HeapSortMax(arr []int) []int {
+// Heap get max  of  slice
+func HeapSortMax(arr []int) {
 	lenth := len(arr)
 	if lenth <= 1 {
-		return arr
+		return
 	}
+	// 树的深度
 	depth := lenth/2 - 1
 	for i := depth; i >= 0; i-- {
 		right := 2*i + 1
 		left := 2*i + 2
+		//防止越界
 		if right < lenth && arr[right] > arr[i] {
 			arr[i], arr[right] = arr[right], arr[i]
 		}
 		if left < lenth && arr[left] > arr[i] {
 			arr[i], arr[left] = arr[left], arr[i]
 		}
-		if right < lenth && left < lenth && arr[right] < arr[left] {
-			arr[right], arr[left] = arr[left], arr[right]
-		}
-
+		// if left < lenth && arr[right] < arr[left] {
+		// 		arr[right], arr[left] = arr[left], arr[right]
+		// 	}
 	}
-	return arr
 }
 
-func main() {
-	a := []int{23, 25, 734, 21, 1, 45, 2, 66, 3, 8, 9, 3, 4, 6}
-	fmt.Println(HeapSortMax(a))
+// HeapSort
+func HeapSort(arr []int) {
+	lenth := len(arr)
+	//不能等于1
+	for i := lenth - 1; i > 0; i-- {
+		fmt.Printf("%s --max:%s--%+v\n", i, arr[i], arr)
+		// 这里特别重要    包括前面 不包括后面...... 这里耽误了很长时间。。。。
+		HeapSortMax(arr[0 : i+1])
+		fmt.Printf("%s --max:%s--%+v\n", i, arr[i], arr)
+		arr[0], arr[i] = arr[i], arr[0]
+		fmt.Printf("%s --max:%s--%+v\n", i, arr[i], arr)
+		fmt.Println()
+	}
+
 }
+
+//
+//func main() {
+//	a := []int{23, 25, 734, 21, 1, 45, 2, 66, 3, 8, 9, 3, 4, 6}
+//	fmt.Println(a)
+//	HeapSort(a)
+//	fmt.Println(a)
+//}
---
+++ Golang/go/Sort/MergeSort.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+//归并操作（merge），也叫归并算法，指的是将两个已经排序的序列合并成一个序列的操作。归并排序算法依赖归并操作。   实质
+
+// 递归   top -- down
+// 迭代   bottom -- up
+
+//迭代
+func MergeSort(arr []int) []int {
+	lenth := len(arr)
+	if lenth < 2 {
+		return arr
+	}
+	middle := len(arr) / 2
+	left := MergeSort(arr[0:middle])
+	right := MergeSort(arr[middle:])
+	return merge(left, right)
+}
+
+func merge(left, right []int) []int {
+	newArray := make([]int, len(left)+len(right))
+	i, j, index := 0, 0, 0
+	for {
+		if left[i] < right[j] {
+			newArray[index] = left[i]
+			i++
+			index++
+		} else {
+			newArray[index] = right[j]
+			j++
+			index++
+		}
+		//
+		if i == len(left) {
+			copy(newArray[index:], right[j:])
+			break
+		}
+		if j == len(right) {
+			copy(newArray[index:], left[i:])
+			break
+		}
+	}
+	return newArray
+
+}
+
+func main() {
+	a := []int{23, 25, 734, 21, 1, 45, 2, 66, 3, 8, 9, 3, 4, 6}
+	fmt.Println(a)
+	fmt.Println(MergeSort(a))
+}
