0a3077cff7278dfcf710335f5ccbc00a905e1d3b
wangxianbiao <422675260@qq.com>
2021-01-06 20:33:09 +0800

    210106

--- Golang/go/Sort/RadixBucketCounterSort.go
+++ Golang/go/Sort/RadixBucketCounterSort.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 //分布式排序   计数排序 --> 桶排序 -->基数排序
 //               数的映射    函数的隐射
@@ -26,7 +29,7 @@ func countingSort(arr []int, minvalue, maxValue int) []int {
 
 }
 
-//十分位桶排序  map
+//分位桶排序  map
 func BucketSort(arr []int, maxValue int) []int {
 	bucketLen := (maxValue / 100) + 1
 	bucket := make([][]int, bucketLen)
@@ -52,7 +55,61 @@ func BucketSort(arr []int, maxValue int) []int {
 
 }
 
-func RadixSortBucket(arr []int) {
+func RadixSort(theArray []int) []int {
+	//获取最大值vl
+	vl := 0
+	for _, v := range theArray {
+		if v > vl {
+			vl = v
+		}
+	}
+	//获取最大值的位数
+	var count int = 0
+	for vl%10 > 0 {
+		vl = vl / 10
+		count++
+	}
+
+	//给桶中对应的位置放数据
+	for i := 0; i < count; i++ {
+		fmt.Println(theArray)
+		theData := int(math.Pow10(i)) //10的i次方
+		//建立空桶
+		var bucket [10][10]int
+		for k := 0; k < len(theArray); k++ {
+			theResidue := (theArray[k] / theData) % 10 //取余
+			var childArray [10]int                     //= bucket[theResidue];//获取子数组
+			for m := 0; m < 10; m++ {
+				if bucket[theResidue][m] == 0 {
+					childArray[m] = theArray[k]
+					bucket[theResidue][m] = childArray[m]
+					break
+				} else {
+					continue
+				}
+			}
+		}
+		//一遍循环完之后需要把数组二维数据进行重新排序，比如数组开始是10 1 18 30 23 12 7 5 18 233 144 ，循环个位数
+		//循环之后的结果为10 30 1 12 23 233 144 5 7 18 18 ，然后循环十位数，结果为1 5 7 10 12 18 18 23 30 233 144
+		//最后循环百位数，结果为1 5 7 10 12 18 18 23 30 144 233
+		var x = 0
+		slice := make([]int, len(theArray))
+		for p := 0; p < len(bucket); p++ {
+			for q := 0; q < len(bucket[p]); q++ {
+				if bucket[p][q] != 0 {
+					slice[x] = bucket[p][q]
+					x++
+				} else {
+					break
+				}
+			}
+		}
+
+		for key, value := range slice {
+			theArray[key] = value
+		}
+	}
+	return theArray
 
 }
 func SelectSort(this []int) {
@@ -79,6 +136,7 @@ func main() {
 	//	a := []int{3, 2, 2, 5, 4, 3, 6, 2, 3, 4, 4, 9, 3, 2, 2, 5, 4, 3, 6, 2, 3, 4, 4, 9, 2, 2, 5, 4, 3, 6, 2, 3, 4, 4, 9, 9, 2, 2, 5, 4, 3, 6, 2, 3, 4, 4, 9, 9, 2, 2, 5, 4, 3, 6, 2, 3, 4, 4, 9, 9, 2, 2, 5, 4, 3, 6, 2, 3, 4, 4, 9, 9, 2, 2, 5, 4, 3, 6, 2, 3, 4, 4, 9}
 	fmt.Println(len(a))
 	// 	b := countingSort(a, 2, 9)
-	b := BucketSort(a, 7123)
+	//b := BucketSort(a, 7123)
+	b := RadixSort(a)
 	fmt.Printf("%+v\n", b)
 }
