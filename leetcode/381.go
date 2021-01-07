54a1f627e8c4c95f53b6a77a3b67ceda879c37fe
wangxianbiao <422675260@qq.com>
2021-01-07 14:25:29 +0800

    210107

--- leetcode/381.go
+++ leetcode/381.go
@@ -1,5 +1,19 @@
 package main
 
+/*
+leetcode 381
+设计一个支持在平均 时间复杂度 O(1) 下， 执行以下操作的数据结构。
+
+注意: 允许出现重复元素。
+
+insert(val)：向集合中插入元素 val。
+remove(val)：当 val 存在时，从集合中移除一个 val。
+getRandom：从现有集合中随机获取一个元素。每个元素被返回的概率应该与其在集合中的数量呈线性相关。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/insert-delete-getrandom-o1-duplicates-allowed
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
 import (
 	"fmt"
 	"math/rand"
